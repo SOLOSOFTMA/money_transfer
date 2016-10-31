74ec176702fee66239c3e5ffa7785ac839334f73
Sione <sione.taumoepeau@gmail.com>
2016-11-01 10:23:32 +1300

    Update EOD Process

--- money_transfer/money_transfer/doctype/eod/eod.py
+++ money_transfer/money_transfer/doctype/eod/eod.py
@@ -5,15 +5,35 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+import MySQLdb
 
 class EOD(Document):
 	
-	def validate(self):
-		self.del_transactions() 
+	def submit(self):
+		self.save_to_temp_table()
+		self.insert_record()
+		self.empty_transactions()
+		self.insert_Opening_Bal()
+		self.empty_temp_table()
+			
+	def save_to_temp_table(self):	
+		frappe.db.sql("""insert into `tabTemp` (user_id, currency, posting_date, inflow) select
+		user_id, currency, posting_date, sum(inflow - outflow) as Total
+		from `tabTransactions Details` GROUP BY user_id DESC, currency ASC""")
 	
-	def del_transactions(self):
-		for tranx in frappe.get_all("Transactions Details", filters={"posting_date": eod_day}):
-		frappe.db.sql("""delete from `tabTransactions Details`
-			where posting_date= %s """,tranx.eod_day)
-		                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+	def insert_record(self):	
+		frappe.db.sql("""insert into `tabTransactions History` (name, modified_by, owner,
+		docstatus, mctn, user_id, currency, posting_date, outflow, inflow, description) select name, modified_by, owner,
+		docstatus, mctn, user_id, currency, posting_date, outflow, inflow, description from `tabTransactions Details`""")	
+
+	def empty_transactions(self):	
+		frappe.db.sql("""delete from `tabTransactions Details`""")
+	
+	def insert_Opening_Bal(self):	
+		frappe.db.sql("""insert into `tabTransactions Details` (name, user_id, currency, posting_date, inflow, description) select name, user_id, 
+		currency, posting_date, inflow, description from `tabTemp`""")
+		
+	def empty_temp_table(self):	
+		frappe.db.sql("""delete from `tabTemp`""")
+	
                                                                                                                   
