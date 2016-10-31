c4800f2f635753374ac4e5f8440f77ae5302d73a
staumoepeau <sione.taumoepeau@gmail.com>
2016-10-31 14:34:24 +1300

    Update eod.py

--- money_transfer/money_transfer/doctype/eod/eod.py
+++ money_transfer/money_transfer/doctype/eod/eod.py
@@ -7,13 +7,5 @@ import frappe
 from frappe.model.document import Document
 
 class EOD(Document):
-	
-	def validate(self):
-		self.del_transactions() 
-	
-	def del_transactions(self):
-		for tranx in frappe.get_all("Transactions Details", filters={"posting_date": eod_day}):
-		frappe.db.sql("""delete from `tabTransactions Details`
-			where posting_date= %s """,tranx.eod_day)
-		                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+	pass                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
                                                                                                                   
